d2cd4741edf26abd4d1af2472ce1188bb58cf04f
BaselTurkman <Basel.Turkman123@gmail.com>
2025-06-12 02:08:51 +0300

    TAABP-17 First Version for a home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,23 @@
+import SearchForm from "@/components/SearchForm";
 import FeaturedDeals from "./components/FeaturedDeals";
 import RecentVisitedHotels from "./components/RecentVisitedHotels";
 import TrendingDestination from "./components/TrendingDestination";
-import { Container, Stack } from "@mui/material";
+import { Box, Container, Stack, Typography } from "@mui/material";
 
 const Home = () => {
   return (
-    <Container sx={{my: 5, p: 5}}>
+    <Container sx={{ my: 5, p: 5 }}>
       <Stack gap={2}>
+        <Box textAlign="center" mb={2}>
+          <Typography variant="h4" fontWeight="bold">
+            Find Your Next Stay
+          </Typography>
+          <Typography variant="subtitle1" color="text.secondary">
+            Discover deals on hotels, homes, and more – tailored for your next
+            adventure.
+          </Typography>
+        </Box>
+        <SearchForm />
         <FeaturedDeals />
         <RecentVisitedHotels />
         <TrendingDestination />
