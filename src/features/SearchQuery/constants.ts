cd23787cfcffe1a1fd74a1b380c6d0dbd275c3b1
BaselTurkman <Basel.Turkman123@gmail.com>
2025-06-13 02:14:24 +0300

    TAABP-19 Add searchQuery to Redux state management

---
+++ src/features/SearchQuery/constants.ts
@@ -0,0 +1,15 @@
+import { SearchQuery } from "@/types";
+
+const today = new Date();
+const tomorrow = new Date();
+tomorrow.setDate(today.getDate() + 1);
+
+export const initialState: SearchQuery = {
+  checkInDate: today.toISOString().split("T")[0],
+  checkOutDate: tomorrow.toISOString().split("T")[0],
+  city: "",
+  numberOfRooms: 1,
+  adults: 2,
+  children: 0,
+  sort: "",
+};
