b12527afc06021a01c035f9a490051ad28fdc206
BaselTurkman <Basel.Turkman123@gmail.com>
2025-06-13 18:21:01 +0300

    TAABP-20 Add Error Boundary

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import "./index.css"
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ErrorBoundary from "./ErrorBoundary/";
 
-createRoot(document.getElementById('root')!).render(
-   <StrictMode>
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <ErrorBoundary>
       <BrowserRouter>
         <Routes>
           <Route path="/*" element={<App />} />
         </Routes>
       </BrowserRouter>
-  </StrictMode>,
-)
+    </ErrorBoundary>
+  </StrictMode>
+);
