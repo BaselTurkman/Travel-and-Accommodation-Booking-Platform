08e736de1df8ce14d69180cc5d93b356db8b564b
BaselTurkman <Basel.Turkman123@gmail.com>
2025-06-14 17:17:28 +0300

    TAABP-21 Improve SearchResult Page

--- src/pages/SearchResult/SearchResult.tsx
+++ src/pages/SearchResult/SearchResult.tsx
@@ -1,18 +1,35 @@
 import SearchForm from "@/components/SearchForm";
 import PageContainer from "@/containers/PageContainer";
 import Hotels from "./components/Hotels";
-import { Stack } from "@mui/material";
+import FilterAmenities from "./components/FilterAmenities";
+import { Grid, Box } from "@mui/material";
 import useGetSearchResultAPI from "./hooks/useGetSearchResultAPI";
+import { AmenitiesProvider } from "./context/AmenitiesProvider";
 
 const SearchResult = () => {
   const { isLoading } = useGetSearchResultAPI();
+
   return (
-    <PageContainer>
-      <Stack gap={2}>
+    <AmenitiesProvider>
+      <PageContainer>
         <SearchForm isLoading={isLoading} />
-        <Hotels />
-      </Stack>
-    </PageContainer>
+        <Grid container spacing={4} alignItems="flex-start">
+          <Grid
+            size={{ xs: 12, md: 3 }}
+            position={{ xs: "static", md: "sticky" }}
+            zIndex={1}
+            top={40}
+          >
+            <FilterAmenities />
+          </Grid>
+          <Grid size={{ xs: 12, md: 9 }}>
+            <Box display="flex" flexDirection="column" gap={2}>
+              <Hotels />
+            </Box>
+          </Grid>
+        </Grid>
+      </PageContainer>
+    </AmenitiesProvider>
   );
 };
 
