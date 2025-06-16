fdd632fe9c6d80110d44f598914603a45c8a981c
BaselTurkman <Basel.Turkman123@gmail.com>
2025-06-16 15:47:21 +0300

    TAABP-28 Handle Filters Based on Amenities

--- src/pages/SearchResult/components/Hotels.tsx
+++ src/pages/SearchResult/components/Hotels.tsx
@@ -5,17 +5,30 @@ import "slick-carousel/slick/slick-theme.css";
 import BaseCardSkeleton from "@/components/Skeletons/BaseCardSkeleton";
 import Hotel from "./Hotel";
 import NoHotelsFound from "./NoHotelsFound";
+import { useAmenities } from "../context/useAmenities";
 
 const Hotels = () => {
   const { searchResult, isLoading } = useGetSearchResultAPI();
-  
-  const renderSearchResult = searchResult.map((hotel) => (
+  const { selectedAmenities } = useAmenities();
+  // Since there isn't much consistency between the IDs, I decided to filter based on the amenity name instead.
+  const selectedAmenityNames = selectedAmenities.map((a) => a.name);
+
+  const filteredHotels = searchResult.filter((hotel) =>
+    hotel.amenities.some((hotelAmenity: { name: string }) =>
+      selectedAmenityNames.includes(hotelAmenity.name)
+    )
+  );
+
+  const hotelsToRender =
+    selectedAmenityNames.length > 0 ? filteredHotels : searchResult;
+
+  const renderSearchResult = hotelsToRender.map((hotel) => (
     <Grid size={{ xs: 12, sm: 6 }} key={hotel.hotelId}>
       <Hotel hotel={hotel} />
     </Grid>
   ));
 
-  const isThereHotels = searchResult.length !== 0;
+  const isThereHotels = hotelsToRender.length !== 0;
 
   return (
     <Box mb={4}>
@@ -35,8 +48,8 @@ const Hotels = () => {
           color="text.secondary"
           align="center"
         >
-          {searchResult.length}{" "}
-          {searchResult.length === 1 ? "hotel found" : "hotels found"}
+          {hotelsToRender.length}{" "}
+          {hotelsToRender.length === 1 ? "hotel found" : "hotels found"}
         </Typography>
       )}
     </Box>
