b9787c4f2b3bd735f63a0e0f7b29f644e50946e7
BaselTurkman <Basel.Turkman123@gmail.com>
2025-06-18 22:44:53 +0300

    TAABP-33 Add safeLazy util

--- src/routes/imports.tsx
+++ src/routes/imports.tsx
@@ -1,9 +1,9 @@
-import { lazy } from "react";
+import { safeLazy } from "./utils/safeLazy";
 
-export const Login = lazy(() => import("@/pages/Login"));
-export const Home = lazy(() => import("@/pages/Home"));
-export const SearchResult = lazy(() => import("@/pages/SearchResult"));
-export const HotelDetails = lazy(() => import("@/pages/HotelDetails"));
-export const Checkout = lazy(() => import("@/pages/Checkout"));
-export const ConfirmationBooking = lazy(() => import("@/pages/ConfirmationBooking"))
-export const Profile = lazy(() => import("@/pages/Profile"))
+export const Login = safeLazy(() => import("@/pages/Login"));
+export const Home = safeLazy(() => import("@/pages/Home"));
+export const SearchResult = safeLazy(() => import("@/pages/SearchResult"));
+export const HotelDetails = safeLazy(() => import("@/pages/HotelDetails"));
+export const Checkout = safeLazy(() => import("@/pages/Checkout"));
+export const ConfirmationBooking = safeLazy(() => import("@/pages/ConfirmationBooking"))
+export const Profile = safeLazy(() => import("@/pages/Profile"))
