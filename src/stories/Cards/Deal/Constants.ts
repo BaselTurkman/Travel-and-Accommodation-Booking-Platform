305e06004adcfef22f7b683f4bd31b5b58393ca5
BaselTurkman <Basel.Turkman123@gmail.com>
2025-06-29 22:31:46 +0300

    TAABP-42 Add Stories for Feature Deal component

---
+++ src/stories/Cards/Deal/Constants.ts
@@ -0,0 +1,14 @@
+import { Deal } from "@/pages/Home/types";
+
+export const baseDeal: Deal = {
+  hotelId: 303,
+  originalRoomPrice: 120.0,
+  discount: 15.0,
+  finalPrice: 102.0,
+  cityName: "Miami",
+  hotelName: "Ocean View Resort",
+  hotelStarRating: 3,
+  title: "Ocean View Retreat",
+  description: "Relax and unwind at this beachfront resort with stunning ocean views.",
+  roomPhotoUrl: "https://d3sak6swcqiwkw.cloudfront.net/media/images/img_5079_0_Fc61f5a.2e16d0ba.fill-750x488.jpg"
+};
