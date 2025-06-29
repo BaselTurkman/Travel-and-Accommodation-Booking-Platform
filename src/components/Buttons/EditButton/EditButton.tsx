788cfc0609168fdd697e92a72b6357a4a5227c0d
BaselTurkman <Basel.Turkman123@gmail.com>
2025-06-29 22:32:04 +0300

    TAABP-42 Add Stories for RecentHotel component

--- src/components/Buttons/EditButton/EditButton.tsx
+++ src/components/Buttons/EditButton/EditButton.tsx
@@ -3,12 +3,18 @@ import EditIcon from "@mui/icons-material/Edit";
 import { FC } from "react";
 import { EditButtonProps } from "./types";
 
-
-const EditButton: FC<EditButtonProps> = ({ onClick, children = "Edit", ...rest }) => {
+const EditButton: FC<EditButtonProps> = ({
+  onClick,
+  children = "Edit",
+  ...rest
+}) => {
   return (
     <Button
       variant="contained"
-      color="warning"
+      sx={{
+        backgroundColor: "#c45500",
+        "&:hover": { backgroundColor: "#a34400" },
+      }}
       startIcon={<EditIcon />}
       onClick={onClick}
       size="medium"
---
+++ src/stories/Cards/RecentHotel/constants.ts
@@ -0,0 +1,13 @@
+import { RecentHotel } from "@/pages/Home/types";
+
+export const baseHotel: RecentHotel = {
+  hotelId: 101,
+  hotelName: "Seaside Paradise",
+  starRating: 4.5,
+  visitDate: "2024-09-15",
+  cityName: "Nice",
+  thumbnailUrl:
+    "https://images.unsplash.com/photo-1600585154340-be6161a56a0c?auto=format&fit=crop&w=800&q=80",
+  priceLowerBound: "180",
+  priceUpperBound: "250",
+};
